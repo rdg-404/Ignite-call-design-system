dc27c6a2bb343c56a676847555ce13beb4e58948
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-24 19:13:09 -0300

    :bricks: get function stitches

--- node_modules/@ignite-ui/react/src/styles/index.ts
+++ node_modules/@ignite-ui/react/src/styles/index.ts
@@ -9,7 +9,16 @@ import {
 } from '@ignite-ui/tokens'
 import { createStitches } from '@stitches/react'
 
-createStitches({
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
   theme: {
     colors,
     fontSizes,
