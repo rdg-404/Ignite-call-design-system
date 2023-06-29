c522d6faa8c725cc7abab3acd75988a68d00413b
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-28 21:03:13 -0300

    :link: export box props

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -1,3 +1,6 @@
 import { styled } from '@stitches/react'
+import { ComponentProps } from 'react'
 
 export const Box = styled('div', {})
+
+export interface BoxProps extends ComponentProps<typeof Box> {}
