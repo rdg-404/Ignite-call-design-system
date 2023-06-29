c772f15bd82ac97f68a36759a268b5d12f4d75bd
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-28 21:11:17 -0300

    :sparkles: create structure box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <p>Testando o elemento box</p>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
