de8e974e9f6a3d2219dc93f6eb5c08aa00351953
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-30 18:24:53 -0300

    :recycle: fix prop elementType

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,8 +1,8 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, ElementType } from 'react'
 import { styled } from '../styles'
 
 export const Button = styled('button', {})
 
 export interface ButtonProps extends ComponentProps<typeof Button> {
-  as?: Element
+  as?: ElementType
 }
