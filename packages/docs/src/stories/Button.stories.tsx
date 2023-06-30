db30b86c64a5b0de8ff0d396a8a6144dc3f7205f
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-30 18:28:55 -0300

    :sparkles: create button stories

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
