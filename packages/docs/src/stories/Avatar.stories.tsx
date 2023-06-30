f0b7e561496c45a7ffdc581e3ce6f692436976f3
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-06-30 17:49:52 -0300

    :sparkles: create avatar stories

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/rdg-404.png',
+    alt: 'Rodrigo Paiva',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
