ec0b983548ca0a5c53776e4647d5ac26eec2f0a6
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-07-01 17:37:05 -0300

    :sparkles: create prefix storie

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -9,3 +9,9 @@ export default {
 } as Meta<TextInputProps>
 
 export const Primary: StoryObj<TextInputProps> = {}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
