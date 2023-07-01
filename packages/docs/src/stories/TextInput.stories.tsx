a4644f3572678c82146b608b6520e6f71a8a8596
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-07-01 17:26:10 -0300

    :bricks: create stories textInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TextInput, TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+
+  args: {},
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {}
