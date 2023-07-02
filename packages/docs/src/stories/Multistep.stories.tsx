e40232fbddf05c9b38e45dc83d63df0f0c58e9c9
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-07-02 16:03:05 -0300

    :sparkles: create multistep stories

---
+++ packages/docs/src/stories/Multistep.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Box, Multistep, MultistepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Multistep',
+  component: Multistep,
+
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultistepProps>
+
+export const Primary: StoryObj<MultistepProps> = {}
